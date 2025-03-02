881f7895a6621df62e9fcef89b0a6f2db0fce57b
BTavkhelidze <beqatavkhelidze4@gmail.com>
2025-03-02 21:37:21 +0400

    change port

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: 'http://localhost:3001',
     credentials: true,
   });
   app.use(cookieParser());
-  await app.listen(8080);
+  await app.listen(3000);
 }
 bootstrap();
