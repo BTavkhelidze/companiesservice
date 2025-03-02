fa829d7bea275894027b58e6ab7f096abd1338ef
BTavkhelidze <beqatavkhelidze4@gmail.com>
2025-03-02 20:54:37 +0400

    fixing

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.memoryUsage());
+  const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: 'http://localhost:3001',
     credentials: true,
   });
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap();
