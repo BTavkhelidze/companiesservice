448d25ce76c09a8391eae2b5b4f13d527779950d
BTavkhelidze <beqatavkhelidze4@gmail.com>
2025-03-02 21:22:06 +0400

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: 'http://localhost:3001',
